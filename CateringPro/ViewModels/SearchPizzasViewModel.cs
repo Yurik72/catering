9a189cfa0c74c1bb13f695208e60767c7bcff200
Yurik72 <42406493+Yurik72@users.noreply.github.com>
2019-08-30 16:31:11 +0300

    Add files via upload

---
+++ CateringPro/ViewModels/SearchPizzasViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CateringPro.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringPro.ViewModels
+{
+    public class SearchPizzasViewModel
+    {
+        [Required]
+        [DisplayName("Serach")]
+        public string SearchText { get; set; }
+
+        //public IEnumerable<string> SearchListExamples { get; set; }
+
+        public IEnumerable<Pizzas> PizzaList { get; set; }
+
+    }
+}
