584ad74ee9af74fe1c1e80dabd2db19f0b8b514e
yurik972 <yurik.kovalenko@gmail.com>
2020-07-15 07:01:43 +0300

    dbupdate user groups

---
+++ CateringPro/Migrations/20200714131250_UserGroup-ID.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CateringPro.Migrations
+{
+    public partial class UserGroupID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserGroupId",
+                table: "UserGroups",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserGrpoupId",
+                table: "UserGroups",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserGroupId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserGrpoupId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_UserGroupId",
+                table: "UserGroups",
+                column: "UserGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserGroupId",
+                table: "AspNetUsers",
+                column: "UserGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_UserGroups_UserGroupId",
+                table: "AspNetUsers",
+                column: "UserGroupId",
+                principalTable: "UserGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_UserGroups_UserGroupId",
+                table: "UserGroups",
+                column: "UserGroupId",
+                principalTable: "UserGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_UserGroups_UserGroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_UserGroups_UserGroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserGroups_UserGroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserGroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserGroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserGrpoupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserGroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserGrpoupId",
+                table: "AspNetUsers");
+        }
+    }
+}
