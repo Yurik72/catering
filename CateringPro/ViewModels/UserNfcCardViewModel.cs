dce814dacfe135739df508af39a77567904d65a0
AndryKovalenko <andry.kovalenko@gmail.com>
2020-08-10 13:42:07 +0300

    UseNfcCards Android

---
+++ CateringPro/ViewModels/UserNfcCardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CateringPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringPro.ViewModels
+{
+    public class UserNfcCardViewModel
+    {
+        public List<CompanyUser> Users { get; set; }
+    }
+}
