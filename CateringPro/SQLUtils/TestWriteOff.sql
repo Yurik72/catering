191569bceafac1e5595b8be5dd7648f60f9a9303
yurik972 <yurik.kovalenko@gmail.com>
2020-08-20 09:31:37 +0300

    fixes

--- CateringPro/SQLUtils/TestWriteOff.sql
+++ CateringPro/SQLUtils/TestWriteOff.sql
@@ -1,12 +1,12 @@
 Declare @iid int
-SET @iid=3
+SET @iid=11
 select * from Ingredients where Id=@iid
 select * from Consignment where IngredientsId=@iid
 select * from ConsignmentMove where IngredientsId=@iid
 
 begin tran
 
-update DocLines set quantity=100 where id=3531
+update DocLines set quantity=100, IngredientsId=@iid where id=3531
 select * from Ingredients where Id=@iid
 select * from Consignment where IngredientsId=@iid
 select * from ConsignmentMove where IngredientsId=@iid
