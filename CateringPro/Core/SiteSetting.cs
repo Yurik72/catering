5bfb112c0221ea4d710ddc549045b965632fe75a
yurik972 <yurik.kovalenko@gmail.com>
2020-08-25 14:05:14 +0300

    localproduction

---
+++ CateringPro/Core/SiteSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringPro.Core
+{
+    public class SiteSetting
+    {
+        public bool EnableLiqPay { get; set; }
+    }
+}
