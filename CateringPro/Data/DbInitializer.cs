3d5056491a8dbd1bf946bac78f0d9df2f17a9ce9
yurik972 <yurik.kovalenko@gmail.com>
2020-09-03 18:30:55 +0300

    merge

--- CateringPro/Data/DbInitializer.cs
+++ CateringPro/Data/DbInitializer.cs
@@ -27,7 +27,7 @@ namespace CateringPro.Data
             //  context.SetCompanyID(1);
             //  var queue = context.DeliveryQueues.Where(dq => dq.UserId == "27fb457f-8b4f-4a66-96ce-5e98ae2f1d91" && dq.DayDate == dayDate.ResetHMS()).ToList();
 
-          
+           
             if (env.EnvironmentName != "LocalProduction")
             {
                 CreateAdminRole(context, roleManager, userManager);
