1e05e4ca7fb688e3c05155a967e1ce98ec137f27
yurik972 <yurik.kovalenko@gmail.com>
2020-09-08 20:37:50 +0300

    fixes

---
+++ CateringPro/Repositories/Complex4AllDay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringPro.Repositories
+{
+    public interface IComplex4AllDayConfig
+    {
+        int ThresholdComplexNumber { get; }
+
+    }
+
+    public class Complex4AllDayConfig : IComplex4AllDayConfig
+    {
+        public int ThresholdComplexNumber { get; set; }
+
+    }
+    public class Complex4AllDay: IDiscountPlugin
+    {
+        private IComplex4AllDayConfig config;
+        public Complex4AllDay()
+        {
+           // config = Configuration.GetSection("Complex4AllDay").Get<Complex4AllDayConfig>();
+        }
+    }
+}
