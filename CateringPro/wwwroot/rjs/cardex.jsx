e85670cacc914debf8a30e84c40a77857ee14b12
yurik972 <yurik.kovalenko@gmail.com>
2021-06-03 18:13:09 +0300

    react

--- CateringPro/wwwroot/rjs/cardex.jsx
+++ CateringPro/wwwroot/rjs/cardex.jsx
@@ -14,28 +14,45 @@ async function doСardFetch(apiurl) {
     }
     
 }
+
 class CardModalDialog extends React.Component {
     constructor(props) {
         super(props);
-       
-
     };
     render() {
+        const { isopen, title, message, onclose, onyes, onno,type } = this.props;
+        const renderbuttons =() => {
+            if (type == 'yesno') {
+                return (
+                    <>
+                        <button id="btnyes" type="button" className="btn btn-primary" onClick={onyes}>Tak</button>
+                        <button id="btno" type="button" className="btn btn-primary" onClick={onno}>Ні</button>
+                    </>
+                )
+            }else {
+                return(
+                        <button id="btnyes" type="button" className="btn btn-primary" onClick={onclose}>Tak</button>
+                    )
+            }
+        }
         return (
-            <div className="modal show" id="ModalPopUp" role="dialog" aria-modal="true" style={{display:"block"}}>
+            <div className={"modal " + (isopen ? "show" : "fade")} id="ModalPopUp" role="dialog" aria-modal="true" style={isopen ? { display: "block" } : {}}>
                 <div className="modal-dialog err-pop" >
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title">Помилка</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <h5 className="modal-title">{title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={onclose}>
                                 <span aria-hidden="true">X</span>
                             </button>
                         </div>
                         <div className="modal-body">
-                            <p>NFC Connection Error</p>
+                            <p>{ message}</p>
                         </div>
                         <div className="modal-footer">
-                            <button id="btnyes" type="button" className="btn btn-primary">Tak</button>
+                            {
+                                renderbuttons()
+                            }
+                            
                         </div>
                     </div>
                 </div>
@@ -51,7 +68,7 @@ class NfcReaderState extends React.Component {
 
     };
     render() {
-        const { isconnected, isconnecting, url, onurlchange,onconnect }  = this.props;
+        const { isconnected, isconnecting, url, onurlchange, commands } = this.props;
         return (
             <div id="nfc_isoffline" className={"row container-state text-white " + (isconnected ? "bg-success":"bg-danger")} >
                 <div className="col-4 container-state">
@@ -78,7 +95,7 @@ class NfcReaderState extends React.Component {
                     </div>
                 </div>
                 <div className="col-4 container-state">
-                    <button id="connectnfc" disabled={isconnecting} className="btn btn-info" onClick={onconnect}>Підключити NFC</button>
+                    <button id="connectnfc" disabled={isconnecting} className="btn btn-info" onClick={(e)=>commands.connect()}>Підключити NFC</button>
                 </div>
             </div>
         );
@@ -111,7 +128,7 @@ class CardList extends React.Component {
      
     }
     render() {
-        const { prop } = this.props;
+        const { prop,commands } = this.props;
         const renderNoRec = (dt) => {
             if (!dt || dt.length == 0) {
                 return <div className="row user-card border rounded">
@@ -169,7 +186,7 @@ class CardList extends React.Component {
                 }
                 {this.state.data.map((item, idx) => {
                     return (
-                        <CardRow key={ idx} userentry={item} />
+                        <CardRow commands={ commands} key={ idx} userentry={item} />
                     )
 
                 })}
@@ -184,7 +201,7 @@ class CardRow extends React.Component{
         this.state = { data: [] };
     }
     render() {
-        const { userentry } = this.props;
+        const { userentry,commands } = this.props;
         return (
             <div key={`${userentry.userId}`} className="row user-card border-bottom text-wrap text-break word-break align-middle" data-id={`${userentry.userId}`}>
                 <div className="col-2 user-card-picture p-2">
@@ -203,7 +220,7 @@ class CardRow extends React.Component{
                     {userentry.cardToken}
                 </div>
                 <div className="col-2 user-card-action  align-middle">
-                    <button className="burn-card btn btn-danger" data-id={`${userentry.userId}`}>Write</button>
+                    <button className="burn-card btn btn-danger" data-id={`${userentry.userId}`} onClick={(e) => commands.burn(userentry.userId)} >Write</button>
                 </div>
             </div>
         )
@@ -212,13 +229,29 @@ class CardRow extends React.Component{
 class CardComp extends React.Component {
     constructor(props) {
         super(props);
-
+        this.noop = () => { };
+        this.noop = this.noop.bind(this);
+        this.dialogstate = { isopen: false, title: '', message: '', type: 'none', yes_cb: this.noop, no_cb: this.noop  };
+       
+        this.state = { dialog: this.dialogstate };
         this.change_url_handler = this.change_url_handler.bind(this);
         this.connect_handler = this.connect_handler.bind(this);
+
         this.nfcsock_status = this.nfcsock_status.bind(this);
         this.nfcsock_cardreaded = this.nfcsock_cardreaded.bind(this);
         this.nfcsock_error = this.nfcsock_error.bind(this);
         this.nfcsock_state = this.nfcsock_state.bind(this);
+        this.setdialog = this.setdialog.bind(this);
+        this.hidedialog = this.hidedialog.bind(this);
+        this.onclosedialog = this.onclosedialog.bind(this);
+        this.onyesdialog = this.onyesdialog.bind(this);
+        this.onnodialog = this.onnodialog.bind(this);
+        this.resetdialog = this.resetdialog.bind(this);
+        this.burn_handler = this.burn_handler.bind(this);
+        this.showdialog = this.showdialog.bind(this);
+        this.showdialog_yes_no = this.showdialog_yes_no.bind(this);
+        this.promise_dialog_yes_no = this.promise_dialog_yes_no.bind(this);
+
         this.state = { url: "wss://localhost:44360/ws", nfcconnected: false, nfcconnecting:false }
         this.nfcsock = new nfc_socket(this.state.url);
 
@@ -226,6 +259,81 @@ class CardComp extends React.Component {
         this.nfcsock.on('cardreaded', this.nfcsock_cardreaded);
         this.nfcsock.on('error', this.nfcsock_error);
         this.nfcsock.on('state', this.nfcsock_state);
+        this.commands = { burn: this.burn_handler, connect: this.connect_handler, showdialog: this.showdialog }
+        //dialog.registerparent(this);
+    }
+    //dialog handling
+    async burn_handler(userid) {
+        console.log(`burn call ${userid}`)
+        //if (!this.nfcsock.isconnected()) {
+        //    this.showdialog('error','NFC ще не підключено.','error');
+        //    return;
+        //}
+        let dialogres = await this.promise_dialog_yes_no('Підтвердіть дію', 'В результаті цієї операції, попередня картка буде недійсна. ');
+        if (dialogres != 'yes')
+            return;
+        const data = await doСardFetch(`/Service/GenUserCardToken?userId=${userid}`);
+        if (!data)
+            return;
+        const writeresult = await this.nfcsock.writecard(json.cardTag);
+        console.log("written");
+    }
+    showdialog(title, message,type) {
+        this.setdialog({ isopen: true, title: title, message: message })
+    }
+    async promise_dialog_yes_no(title, message) {
+        try {
+            var promisedlg = new Promise((resolve, reject) => {
+                this.showdialog_yes_no(title, message, resolve, reject)
+            });
+            var res = await promisedlg;
+            return res;
+        }
+        catch (ex) {
+            return ''
+        }
+    }
+    showdialog_yes_no(title, message, yesCallback, noCallback) {
+        this.setdialog({ isopen: true, title: title, message: message, type: 'yesno', yes_cb: yesCallback, no_cb: noCallback })
+    }
+    setdialog(dlg) {
+        this.setState({ dialog: dlg })
+    }
+    resetdialog() {
+        //dialog.resethandlers()
+    }
+    hidedialog() {
+        this.setdialog(this.dialogstate)
+        this.resetdialog();
+    }
+    onclosedialog(e) {
+        e.preventDefault();
+        const { yes_cb, no_cb } = this.state.dialog;
+        //probably we are waiting promise
+        if (no_cb)
+            no_cb("no");
+        if (yes_cb)
+            yes_cb("no");
+        //dialog.callclose();
+        this.hidedialog();
+    }
+    onyesdialog(e) {
+        e.preventDefault();
+        //this is a promise
+        const { yes_cb } = this.state.dialog;
+        if (yes_cb)
+            yes_cb("yes");
+        this.hidedialog();
+    }
+    onnodialog(e) {
+        e.preventDefault();
+        const { yes_cb, no_cb } = this.state.dialog;
+        //to call no will raise error
+       // if (yes_cb)
+       //     yes_cb("no");
+        if (no_cb)
+            no_cb("no");
+        this.hidedialog();
     }
     nfcsock_status(msg) {
         console.log(msg)
@@ -237,6 +345,8 @@ class CardComp extends React.Component {
     nfcsock_error(msg) {
         console.log("nfcsock_error")
         console.log(msg)
+        this.showdialog("Connection Error", msg)
+        //dialog.showdialog("Connection Error",msg)
     }
     nfcsock_state(msg) {
         console.log(msg)
@@ -251,18 +361,24 @@ class CardComp extends React.Component {
     }
     render() {
         const compprops = {};
-        
+        const { dialog } = this.state;
+        const renderDialog =(dlg) => {
+            if (dlg && dlg.isopen) {
+                return (
+                    <CardModalDialog isopen={dlg.isopen} title={dlg.title} message={dlg.message} type={ dlg.type}  onclose={this.onclosedialog} onyes={this.onyesdialog} onno={this.onnodialog} />
+                    )
+            }
+        }
         return (
             <div>
                 <header className="main-header"><h1>Керування картками користувачів</h1> </header>
-                <NfcReaderState isconnected={this.state.nfcconnected}
+                <NfcReaderState commands={this.commands} isconnected={this.state.nfcconnected}
                     isconnecting={this.state.nfcconnecting}
                     url={this.state.url}
                     onurlchange={this.change_url_handler}
-                    onconnect={this.connect_handler }
                 />
-                <CardList />
-             
+                <CardList commands={this.commands} />
+                {renderDialog(dialog)}
             </div>
         );
 
---
+++ CateringPro/wwwroot/rjs/dialogbridge.jsx
@@ -0,0 +1,30 @@
+﻿class DialogBridge {
+    constructor(comp) {
+        this.component = {}
+        this.noop = () => { };
+        this.closehandle = this.noop
+        this.yeshandle = this.noop
+        this.nohandle = this.noop
+    }
+    registerparent(comp) {
+        this.component = comp;
+    }
+    showdialog(title, message) {
+        this.component.setdialog({ isopen: true, title: title, message: message })
+    }
+    resethandlers() {
+        this.closehandle = this.noop
+        this.yeshandle = this.noop
+        this.nohandle = this.noop
+    }
+    callclose() {
+        this.closehandle();
+    }
+    callyes() {
+        this.yeshandle();
+    }
+    callno() {
+        this.nohandle();
+    }
+}
+var dialog = new DialogBridge();
